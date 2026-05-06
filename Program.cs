f5c5eea475fc99ee200f15903fdbdec910cb7294
BenjaminSchroeders <benjamin.schroeders@icloud.com>
2026-05-06 22:41:28 +0200

    Mise en place du système personnage de base

--- Program.cs
+++ Program.cs
@@ -1,1 +1,41 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+using System.Dynamic;
+
+class Character
+{
+    public string Nom { get; private set; }
+    public int Health { get; private set; }
+    public int AttackPower { get; private set; }
+
+    public Character(string nom, int health, int attackPower)
+    {
+        Nom = nom;
+        Health = health;
+        AttackPower = attackPower;
+
+    }
+
+    public void TakeDamage(int damage)
+    {
+        Health -= damage;
+    }
+}
+
+class Player : Character
+{
+    public Player(string nom, int health, int attackPower)
+        : base(nom, health, attackPower)
+    {
+
+    }
+}
+
+class Ennemy : Character
+{
+
+    public Ennemy(string nom, int health, int attackPower, int damage)
+        : base(nom, health, attackPower)
+    {
+
+    }
+}

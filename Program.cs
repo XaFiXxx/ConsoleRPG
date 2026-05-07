5bf72b3ac37ff3f65eb56c879702f4b44dbfb47e
BenjaminSchroeders <benjamin.schroeders@icloud.com>
2026-05-07 18:16:25 +0200

    Système de combat contrôlé par le joueur + Potion de vie (infinie actuellement)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel;
 using System.Dynamic;
+using System.Security.Cryptography.X509Certificates;
 
 class Character
 {
@@ -17,26 +18,42 @@ class Character
     public void TakeDamage(int damage)
     {
         Health -= damage;
+
+        if (Health <= 0)
+        {
+            Health = 0;
+        }
     }
 
     public void Attack(Character target)
     {
         target.TakeDamage(AttackPower);
+        Console.WriteLine($"{Nom} attaque {target.Nom}");
+    }
+
+    public void Heal(int amountHeal)
+    {
+        Health += amountHeal;
+    }
+
+    public bool IsDead()
+    {
+        return Health <= 0;
     }
 }
 
 class Player : Character
 {
-    public Player(string nom, int health, int attackPower)
+    public int AmountHeal { get; private set; }
+    public Player(string nom, int health, int attackPower, int amountHeal)
         : base(nom, health, attackPower)
     {
-
+        AmountHeal = amountHeal;
     }
 }
 
 class Ennemy : Character
 {
-
     public Ennemy(string nom, int health, int attackPower)
         : base(nom, health, attackPower)
     {
@@ -46,14 +63,60 @@ class Ennemy : Character
 
 class Program
 {
-
     static void Main()
     {
-        Player player = new Player("Benjamin", 100, 20);
-        Ennemy zombie = new Ennemy("Zombie", 50, 10);
+        Player player = new Player("Benjamin", 100, 20, 20);
+        Ennemy zombie = new Ennemy("Zombie", 150, 10);
 
-        player.Attack(zombie);
+        Console.WriteLine($"Bienvenue dans mon mini RPG {player.Nom}");
 
-        Console.WriteLine(zombie.Health);
+
+        while (!player.IsDead() && !zombie.IsDead())
+        {
+            Console.WriteLine("1. Attaquer l'ennemi");
+            Console.WriteLine("2. Prendre une potion");
+            Console.WriteLine("4. FUIR...!");
+
+            string input = Console.ReadLine() ?? "";
+
+
+            switch (input)
+            {
+                case "1":
+                    player.Attack(zombie);
+                    Console.WriteLine($"{zombie.Nom} HP : {zombie.Health}");
+
+                    if (zombie.IsDead())
+                        break;
+
+                    zombie.Attack(player);
+                    Console.WriteLine($"{player.Nom} HP : {player.Health}");
+                    break;
+
+                case "2":
+                    player.Heal(player.AmountHeal);
+                    Console.WriteLine($"{player.Nom} HP : {player.Health}");
+                    break;
+
+                case "4":
+                    Console.WriteLine("Tu as quitté le combat..!");
+                    return;
+
+                default:
+                    Console.WriteLine("❌ Choix invalide..!");
+                    break;
+            }
+
+
+        }
+
+        if (player.IsDead())
+        {
+            Console.WriteLine($"{player.Nom} est mort !");
+        }
+        else
+        {
+            Console.WriteLine($"{zombie.Nom} est mort ! ☠️");
+        }
     }
 }
